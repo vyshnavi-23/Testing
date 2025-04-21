83460207ddea04e140d9fc26258fabe405d94182
Vyshnavi C V <141933663+vyshnavi-23@users.noreply.github.com>
2025-04-21 14:01:07 +0530

    ParabankFundTransfer.java

--- ParabankFundTransfer.java
+++ ParabankFundTransfer.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 public class ParabankFundTransfer {
     WebDriver driver;
 
-    // Locators
     By transferFundsLink = By.xpath("//a[normalize-space()='Transfer Funds']");
     By amountField = By.xpath("//input[@id='amount']");
     By transferButton = By.xpath("//input[@value='Transfer']");
@@ -14,19 +13,18 @@ public class ParabankFundTransfer {
     By findByAmountField = By.xpath("//input[@id='amount']");
     By findByAmountButton = By.xpath("//button[@id='findByAmount']");
 
-    // Constructor
     public ParabankFundTransfer(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Method to perform fund transfer
+    
     public void transferFunds(String amount) {
         driver.findElement(transferFundsLink).click();
         driver.findElement(amountField).sendKeys(amount);
         driver.findElement(transferButton).click();
     }
 
-    // Method to search for the transaction
+    
     public void findTransactionByAmount(String amount) {
         driver.findElement(findTransactionsLink).click();
         driver.findElement(findByAmountField).sendKeys(amount);
